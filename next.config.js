b932ac9856136ec99540ec479ebfc8531fa705b1
harikrishnan <hari@kabali.io>
2024-11-29 22:32:49 +0530

    final changes

--- next.config.js
+++ next.config.js
@@ -1,27 +1,14 @@
-import { withSentryConfig } from '@sentry/nextjs';
-
+import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // Adding the export output as required
-};
+const nextConfig = {};
 
-// Sentry configuration wrapper
-const sentryConfig = {
-  silent: true,
-  org: "javascript-mastery",
-  project: "javascript-nextjs",
-};
-
-const sentryWebpackPluginOptions = {
-  widenClientFileUpload: true,
-  transpileClientSDK: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-};
-
-export default withSentryConfig(
-  nextConfig,
-  sentryConfig,
-  sentryWebpackPluginOptions
-);
+export default withSentryConfig(nextConfig, {
+silent: true,
+org: "javascript-mastery",
+project: "javascript-nextjs",
+}, {
+widenClientFileUpload: true,
+transpileClientSDK: true,
+hideSourceMaps: true,
+disableLogger: true,
+});
