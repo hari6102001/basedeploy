8f461c56cf30a50ff00f3778fb2e10c387c41881
harikrishnan <hari@kabali.io>
2024-11-29 21:50:11 +0530

    base changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import {withSentryConfig} from '@sentry/nextjs';
 import { output } from 'three/webgpu';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
+    output:"export"
 };
 
 export default withSentryConfig(nextConfig, {
